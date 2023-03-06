8c5f8c8c916a8998723d418dcd4e36ecae573d72
Mark Szulc <mszulc@adobe.com>
2023-03-06 23:16:17 +1100

    style tweaks to hero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,10 +23,15 @@ window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information 
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const subhead = main.querySelector('h4');
+  const cta = document.createElement('p');
+  cta.className = "cta";
+  cta.innerHTML = 'Schedule a consultation';
+  
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h1, subhead, cta] }));
     main.prepend(section);
   }
 }
