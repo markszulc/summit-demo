2f05cb819e78a20ebb246512826dea57191b2aac
Mark Szulc <mszulc@adobe.com>
2023-03-06 15:23:49 +1100

    initial styling

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -103,16 +103,22 @@ export default async function decorate(block) {
     nav.id = 'nav';
     nav.innerHTML = html;
 
-    const classes = ['brand', 'sections', 'tools'];
+    const classes = ['brand', 'search', 'sections','auth'];
     classes.forEach((c, i) => {
       const section = nav.children[i];
       if (section) section.classList.add(`nav-${c}`);
     });
 
+     
     const navSections = nav.querySelector('.nav-sections');
     if (navSections) {
       navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+        if (navSection.querySelector('ul')) {
+          navSection.classList.add('nav-drop');
+          const downchevron = document.createElement("div");
+          downchevron.innerHTML = '<p>v</p>';
+          navSection.append(downchevron);
+        }
         navSection.addEventListener('click', () => {
           if (isDesktop.matches) {
             const expanded = navSection.getAttribute('aria-expanded') === 'true';
@@ -123,18 +129,18 @@ export default async function decorate(block) {
       });
     }
 
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-        <span class="nav-hamburger-icon"></span>
-      </button>`;
-    hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-    nav.prepend(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    // prevent mobile nav behavior on window resize
-    toggleMenu(nav, navSections, isDesktop.matches);
-    isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
+    // // hamburger for mobile
+    // const hamburger = document.createElement('div');
+    // hamburger.classList.add('nav-hamburger');
+    // hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
+    //     <span class="nav-hamburger-icon"></span>
+    //   </button>`;
+    // hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
+    // nav.prepend(hamburger);
+    // nav.setAttribute('aria-expanded', 'false');
+    // // prevent mobile nav behavior on window resize
+    // toggleMenu(nav, navSections, isDesktop.matches);
+    // isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
 
     decorateIcons(nav);
     const navWrapper = document.createElement('div');
