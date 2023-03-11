1d3d8d4cb9420dd95775f71adceb8b3d0ed2e1ae
Mark Szulc <mszulc@adobe.com>
2023-03-11 14:29:42 +1100

    General Styling updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,7 +28,7 @@ function buildHeroBlock(main) {
   const subhead = main.querySelector('h4');
   const cta = document.createElement('p');
   cta.className = "cta";
-  cta.innerHTML = 'Schedule a consultation';
+  cta.innerHTML = '<span class="evo-button__content"><span class="evo-button__content__copy">Schedule a consultation</span><span class="evo-button__content__icon ml-2"><evo-icon name="arrow-right"><!----><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" class="icon icon-md icon-fill-text icon-stroke-text " style="" viewBox="0 0 24 24"><path class="icon-arrow-right__arrow" d="M0,7.5v-1A.477.477,0,0,1,.45,6H15L11,1.558a.54.54,0,0,1,0-.71l.639-.7a.419.419,0,0,1,.639,0L17.8,6.28a.8.8,0,0,1,.2.53v.38a.816.816,0,0,1-.2.53l-5.526,6.132a.419.419,0,0,1-.639,0L11,13.142a.529.529,0,0,1,0-.7L15,8H.45A.477.477,0,0,1,0,7.5Z" transform="translate(3 5)"></path></svg></evo-icon></span></span>';
   const herocontent = document.createElement("div");
   herocontent.classList.add('banner-content');
   herocontent.append(h1);
